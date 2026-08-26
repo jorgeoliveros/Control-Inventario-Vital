1bd4fa70c451b39a26f8939d9218f3703988141e
jorgeoliveros <jotamov01@gmail.com>
2026-08-26 15:57:52 -0600

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -3,7 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ihlhmpsclyjysoawvaua.supabase.co';
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+// Manejo seguro para evitar que el empaquetador Minified rompa la función de inicialización
+const createClientFn = typeof createClient === 'function' ? createClient : (createClient as any).default || (createClient as any).createClient;
+
+export const supabase = createClientFn(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: false
   }
